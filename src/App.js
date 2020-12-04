3415e0e01d3ea4766b958c7f6615c54c2f7a8121
Diego Alberto Costa <d.costa1990@outlook.com>
2020-12-04 22:35:04 +0100

    cards and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import fetchData from './api/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styles from './App.module.css';
+
+class App extends Component {
+    state = {
+        data: {},
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData });
+    }
+    render() {
+        const{ data } = this.state;
+        return (
+            <div className={styles.container}>
+                <Cards data={data} />
+                <CountryPicker />
+                <Chart />
+            </div>
+        );
+    }
 }
 
 export default App;
